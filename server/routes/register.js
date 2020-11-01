ca65cd295892adff24d7e8ccbf3ee368b78bc47a
Eric <ericgrosse1@gmail.com>
2020-11-01 12:32:28 -0500

    Updated POST /register endpoint

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,12 +3,18 @@ const User = require('../models/User')
 const router = express.Router()
 
 router.post('/', (req, res) => {
+  if(!req.body.username || !req.body.password) {
+    res.status(400).send({error: 'missing username and/or password in payload'})
+    return
+  }
+
   const user = new User(req.body)
+
   user.save((err) => {
     if (err) {
       return console.error(err)
     } 
-    res.status(201).send(user)
+    res.status(201).send({username: req.body.username})
   })
 })
 
