4b9d80000fc72026c59efba1296b76a09177b2cf
Eric <ericgrosse1@gmail.com>
2020-11-01 17:38:36 -0500

    Added GET /user/:userId endpoint

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,21 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 
+router.get('/:userId', async (req, res) => {
+  try {
+    let user = await User
+    .findById(req.params.userId)
+    .select('username posts')
+    //.populate('something')
+    .exec()
+    
+    res.status(200).json(user)
+  }
+  catch (err) {
+    res.status(500).json({error: err})
+  }
+})
+
 router.post('/register', async (req, res) => {
   const user = new User(req.body)
 
@@ -9,7 +24,7 @@ router.post('/register', async (req, res) => {
     await user.save()
     res.status(201).end()
   } catch (err) {
-    res.status(201).json({error: err})
+    res.status(500).json({error: err})
   }    
 })
 
