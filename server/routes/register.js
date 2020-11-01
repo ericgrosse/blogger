d6a0e39b0e30c632fdd0cd6c47bef544d301a038
Eric <ericgrosse1@gmail.com>
2020-11-01 14:25:25 -0500

    Updated User schema

--- server/routes/register.js
+++ server/routes/register.js
@@ -2,20 +2,15 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 
-router.post('/', (req, res) => {
-  if(!req.body.username || !req.body.password) {
-    res.status(400).send({error: 'missing username and/or password in payload'})
-    return
-  }
+router.post('/', async (req, res) => {
+  const user = new User({...req.body, posts: []})
 
-  const user = new User(req.body)
-
-  user.save((err) => {
-    if (err) {
-      return console.error(err)
-    } 
-    res.status(201).send({username: req.body.username})
-  })
+  try {
+    await user.save()
+    res.status(201).send({username: req.body.username, posts: []})
+  } catch (err) {
+    res.status(201).send({error: err})
+  }    
 })
 
 module.exports = router
