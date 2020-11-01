b015baf446e531f4be4c05f89f6d91ebbabe0fa9
Eric <ericgrosse1@gmail.com>
2020-11-01 10:42:59 -0500

    Removed boilerplate, setup registration page

---
+++ src/views/Register.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <div class="register-inputs">
+      <input type="text" placeholder="Username">
+      <input type="text" placeholder="Password">
+      <input type="text" placeholder="Confirm password">
+      <button>Submit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.register {
+  max-width: 300px;
+  margin: 0 auto;
+  
+  h1 {
+    text-align: center;
+  }
+}
+
+.register-inputs {
+  display: flex;
+  flex-direction: column;
+}
+</style>
