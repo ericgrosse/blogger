43ca5d138cc7c4adb340848661cc9ff2861bcc1f
Eric <ericgrosse1@gmail.com>
2020-11-01 18:35:54 -0500

    Added GET /posts/:postId endpoint

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,6 +3,20 @@ const Post = require('../models/Post')
 const User = require('../models/User')
 const router = express.Router()
 
+router.get('/:postId', async (req, res) => {
+  try {
+    let post = await Post
+    .findById(req.params.postId)
+    .select('title content userId')
+    .exec()
+    
+    res.status(200).json(post)
+  }
+  catch (err) {
+    res.status(500).json({error: err})
+  }
+})
+
 router.post('/', async (req, res) => {
   const post = new Post(req.body)
 
