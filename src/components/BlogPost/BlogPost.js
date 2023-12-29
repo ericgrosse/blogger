20316e68c41198c982f3236656427b6a5c16514c
ericgrosse <ericgrosse1@gmail.com>
2023-12-29 12:52:26 -0500

    Updated BlogPost component to destructure props

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -6,13 +6,13 @@ import { faEye, faEdit, faTrashCan } from '@fortawesome/free-regular-svg-icons';
 import ConfirmDeleteModal from '../ConfirmDeleteModal/ConfirmDeleteModal'; // Adjust the path based on your project structure
 import './BlogPost.scss';
 
-function BlogPost(props) {
+function BlogPost({ post, editable }) {
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const navigate = useNavigate();
-  const isCurrentUser = localStorage.getItem('username') === props.post.user.username;
+  const isCurrentUser = localStorage.getItem('username') === post.user.username;
 
   const handleEditBlogPost = () => {
-    navigate(`/${props.post.user.username}/${props.post._id}/edit-post`);
+    navigate(`/${post.user.username}/${post._id}/edit-post`);
   };
 
   const handleDeleteBlogPost = () => {
@@ -25,7 +25,7 @@ function BlogPost(props) {
 
   return (
     <div className="BlogPost">
-      {isCurrentUser && props.editable && (
+      {isCurrentUser && editable && (
         <div className="action-buttons">
           <button className="edit-button" onClick={handleEditBlogPost}>
             <FontAwesomeIcon icon={faEdit} />
@@ -36,27 +36,27 @@ function BlogPost(props) {
         </div>
       )}
       <div className="title-container">
-        <Link to={`/${props.post.user.username}/${props.post._id}`} style={{ textDecoration: 'none' }}>
-          <h1 className="title">{props.post.title}</h1>
+        <Link to={`/${post.user.username}/${post._id}`} style={{ textDecoration: 'none' }}>
+          <h1 className="title">{post.title}</h1>
         </Link>
       </div>
       <div>
-        <Link to={`/${props.post.user.username}/posts`} style={{ textDecoration: 'none' }}>
-          <p className="author">By {props.post.user.displayName} ({`@${props.post.user.username}`})</p>
+        <Link to={`/${post.user.username}/posts`} style={{ textDecoration: 'none' }}>
+          <p className="author">By {post.user.displayName} ({`@${post.user.username}`})</p>
         </Link>
       </div>
-      <p className="date-published">Date Published: {new Date(props.post.datePublished).toLocaleString()}</p>
-      {props.post.dateLastEdited && (
-        <p className="date-edited">Date Last Edited: {new Date(props.post.dateLastEdited).toLocaleString()}</p>
+      <p className="date-published">Date Published: {new Date(post.datePublished).toLocaleString()}</p>
+      {post.dateLastEdited && (
+        <p className="date-edited">Date Last Edited: {new Date(post.dateLastEdited).toLocaleString()}</p>
       )}
       <div className="markdown">
-        <ReactMarkdown>{props.post.content}</ReactMarkdown>
+        <ReactMarkdown>{post.content}</ReactMarkdown>
       </div>
-      <p className="view-count"><FontAwesomeIcon icon={faEye} /> {props.post.viewCount}</p>
+      <p className="view-count"><FontAwesomeIcon icon={faEye} /> {post.viewCount}</p>
       
       <ConfirmDeleteModal
         title="Delete Blog Post"
-        post={props.post}
+        post={post}
         isOpen={deleteModalOpen}
         onClose={handleCancelDelete}
       />
